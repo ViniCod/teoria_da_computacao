267a556ac3a6e1cada1e58038840a0aab6368ebd
Vinicius <viniciusprates.6798@aluno.saojudas.br>
2021-09-02 21:42:45 -0300

    exercícios de automatos finitos definidos

---
+++ automato_finito_definido/AfdExe3.java
@@ -0,0 +1,36 @@
+public class AfdExe3 {
+
+    public static void main (String[] args) {
+        String entrada = "abba";
+        int estado = 0;
+        int posicao = 0;
+
+        while (posicao < entrada.length()) {
+
+            char elemento = entrada.charAt(posicao);
+
+            System.out.println(
+                entrada.substring(0, posicao)+"|q"+estado +"|"+entrada.substring(posicao)
+            );
+
+            if (estado == 0 && elemento == 'a') {
+                estado = 1;
+            } else if (estado == 0 && elemento == 'b') {
+                estado = 0;
+            } else if (estado == 1 && elemento == 'a') {
+                estado = 0;
+            } else if (estado == 1 && elemento == 'b') {
+                estado = 1;
+            }
+            posicao++;
+        }
+
+        System.out.println(
+            entrada.substring(0, posicao)+"|q"+estado+"|"+entrada.substring(posicao)
+        );
+        if (estado == 1) System.out.println("aceita");
+        else System.out.println("rejeitada");
+
+    }
+
+}
